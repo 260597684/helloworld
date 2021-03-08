f318be59755fb4cd615ac2af256dfdcf93739e3d
王鑫 <260597684@qq.com>
2021-03-08 17:24:26 +0800

    123

--- myui/src/01宏任务微任务.js
+++ myui/src/01宏任务微任务.js
@@ -1,4 +1,11 @@
 /**
+ * 事件循环：
+ * 同步任务和异步任务进入不同的执行环境，同步的进入主线程（主执行栈），异步的进入任务队列
+ * 主线程执行完毕之后，去任务队列中读取对应的任务，推入 主线程
+ * 上述说到不停重复的过程就是事件循环
+ */
+
+/**
  * 宏任务：整体代码，settimeout，setinterval， I/O, UIrendering
  * 微任务：promise，Object.observer,
  * 优先级：process.nexttick> promise.then>settimeout
--- myui/src/test3.js
+++ myui/src/test3.js
@@ -1,16 +1,16 @@
-console.log('script start');
+// console.log('script start');
 
-setTimeout(function() {
-  console.log('setTimeout');
-}, 0);
+// setTimeout(function() {
+//   console.log('setTimeout');
+// }, 0);
 
-Promise.resolve().then(function() {
-  console.log('promise1');
-}).then(function() {
-  console.log('promise2');
-});
+// Promise.resolve().then(function() {
+//   console.log('promise1');
+// }).then(function() {
+//   console.log('promise2');
+// });
 
-console.log('script end');
+// console.log('script end');
 
 
 // script start
@@ -18,3 +18,20 @@ console.log('script end');
 // promise1    
 // promise2    
 // setTimeout  
+
+
+console.log('script start');
+
+setTimeout(function() {
+  console.log('timeout1');
+}, 10);
+
+new Promise(resolve => {
+    console.log('promise1');
+    resolve();
+    setTimeout(() => console.log('timeout2'), 10);
+}).then(function() {
+    console.log('then1')
+})
+
+console.log('script end');
